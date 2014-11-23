c1592e63784c0b3289df006a5acccb51f9d68dd7
boxcarhobo <n.tauch@t-online.de>
2014-11-23 23:37:38 +0100

    Extended the data scraper method

--- Dokumente/Python/Webscraper/search.py
+++ Dokumente/Python/Webscraper/search.py
@@ -1,11 +1,19 @@
 import urllib.request
+import time
+import re
 
 from bs4 import BeautifulSoup
 
 
+#Performs a HTTP-'POST' request, passes it to BeautifulSoup and returns the result
+def doRequest(request):
+    requestResult = urllib.request.urlopen(request)
+    soup = BeautifulSoup(requestResult)
+    return soup
+
 
 #Returns all the result links from the given search parameters
-def getLinks(plz_von, plz_bis):
+def getLinksFromSearch(plz_von, plz_bis):
     database = []
     links = []
 
@@ -50,14 +58,69 @@ def getLinks(plz_von, plz_bis):
     return links
 
 
-#Performs a HTTP-'POST' request, passes it to BeautifulSoup and returns the result
-def doRequest(request):
-    requestResult = urllib.request.urlopen(request)
-    soup = BeautifulSoup(requestResult)
-    return soup
 
-links = getLinks(50000, 50126)
 
-print(links)
+def searchOnLinks(links):
+    adresses = []
+    for item in links:
+        adresses.append(getContactInfoFromPage(item))
+        time.sleep(0.3)
 
-        
+    return adresses
+
+def getContactInfoFromPage(page):
+    name = ''
+    straße = ''
+    plz = ''
+    stadt = ''
+    telefon = ''
+    mail = ''
+    url = ''
+
+    data = [
+           #'Name',
+           #'Straße',
+           #'PLZ',
+           #'Stadt',
+           #'Telefon',
+           #'E-Mail',
+           #'Homepage'
+            ]
+    
+    request = urllib.request.Request("http://www.altenheim-adressen.de/schnellsuche/" + page)
+    request.add_header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+    request.add_header("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:33.0) Gecko/20100101 Firefox/33.0")
+    soup = doRequest(request)
+
+    name = getFieldValue(soup, "Name")
+    data.append(name)
+
+    straße = getFieldValue(soup, "Straße")
+    data.append(straße)
+
+    ort = getFieldValue(soup, "Ort")
+    (plz, stadt) = ort.split(' ', 1)
+    data.append(plz)
+    data.append(stadt)
+
+    telefon = getFieldValue(soup, "Telefon")
+    data.append(telefon)
+
+    mail = getFieldValue(soup, "EMail")
+    data.append(mail)
+
+    url = getFieldValue(soup, "Internetadresse")
+    data.append(url)
+
+    return data
+    
+
+def getFieldValue(soup, field):
+    field_label = soup.find('td', text=field + ':')
+    return field_label.find_next_sibling('td').get_text(strip=True)
+
+links = getLinksFromSearch(50000, 50126)
+
+data = searchOnLinks(links)
+
+print(data)
